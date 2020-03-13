b5d3e947e95b550f9e771c8e3f6975489eb90454
Greg Pauloski <gpauloski@yahoo.com>
2020-03-13 09:47:54 -0500

    Updated training scripts
    Former-commit-id: dde22177aa6c63a4d91b1b3437c5bd2e09b1759a

--- sbatch/cp_imagenet_to_temp.sh
+++ sbatch/cp_imagenet_to_temp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#echo "Copying imagenet.tar to /tmp/"
-#cp /scratch/05714/jgpaul/imagenet.tar /tmp/
-#echo "Done copying. Extracting"
-#tar xf /tmp/imagenet.tar -C /tmp/ --strip-components 5
-echo "Copying imagenet to /tmp"
-cp -r /scratch/05714/jgpaul/imagenet/* /tmp/
-echo "Done"
+echo "Copying imagenet.tar to /tmp/"
+cp /scratch/05714/jgpaul/imagenet.tar /tmp/
+echo "Done copying. Extracting"
+tar xf /tmp/imagenet.tar -C /tmp/ --strip-components 5
+#echo "Copying imagenet to /tmp"
+#cp -r /scratch/05714/jgpaul/imagenet/* /tmp/
+#echo "Done"
