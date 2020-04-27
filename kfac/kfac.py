fd8a0c38ba168b0429c6314dbf8dc918f8559c13
Greg Pauloski <gpauloski@yahoo.com>
2020-04-27 13:24:22 -0500

    Update kfac.py
    Former-commit-id: b2fe96e00285c6c252b8912d49b2c0158515fe02

--- kfac/kfac.py
+++ kfac/kfac.py
@@ -3,7 +3,7 @@ import math
 import torch
 import torch.optim as optim
 
-from kfac_utils import (ComputeCovA, ComputeCovG)
+from kfac_utils import (ComputeA, ComputeG)
 from kfac_utils import update_running_stat
 from kfac_utils import try_contiguous
 from kfac_utils import cycle
@@ -15,38 +15,39 @@ class KFAC(optim.Optimizer):
     def __init__(self,
                  model,
                  lr=0.1,
-                 stat_decay=0.95,
+                 factor_decay=0.95,
                  damping=0.001,
                  kl_clip=0.001,
-                 cov_update_freq=10,
+                 fac_update_freq=10,
                  inv_update_freq=100,
                  batch_averaged=True,
                  diag_blocks=1,
                  diag_warmup=0,
                  distribute_layer_factors=None):
 
-        defaults = dict(lr=lr, damping=damping, cov_update_freq=cov_update_freq,
+        defaults = dict(lr=lr,
+                        damping=damping,
+                        fac_update_freq=fac_update_freq,
                         inv_update_freq=inv_update_freq) 
-        super(KFAC, self).__init__(model.parameters(), defaults)
-        self.CovAHandler = ComputeCovA()
-        self.CovGHandler = ComputeCovG()
 
+        super(KFAC, self).__init__(model.parameters(), defaults)
+
+        self.computeA = ComputeA()
+        self.computeG = ComputeG()
         self.known_modules = {'Linear', 'Conv2d'}
         self.modules = []
-
-        self.model = model
-        self._prepare_model()
+        self._register_modules(model)
 
         self.steps = 0
 
         self.m_a, self.m_g = {}, {}
-        self.m_aa, self.m_gg = {}, {}
-        self.Q_a, self.Q_g = {}, {}
-        self.d_a, self.d_g = {}, {}
+        self.m_A, self.m_G = {}, {}
+        self.m_QA, self.m_QG = {}, {}
+        self.m_dA, self.m_dG = {}, {}
 
-        self.stat_decay = stat_decay
+        self.factor_decay = factor_decay
         self.kl_clip = kl_clip
-        self.cov_update_freq = cov_update_freq
+        self.fac_update_freq = fac_update_freq
         self.inv_update_freq = inv_update_freq
         self.diag_blocks = diag_blocks
         self.diag_warmup = diag_warmup
@@ -63,14 +64,14 @@ class KFAC(optim.Optimizer):
         self.rank_iter = cycle(list(range(hvd.size())))
 
     def _save_input(self, module, input):
-        if torch.is_grad_enabled() and self.steps % self.cov_update_freq == 0:
+        if torch.is_grad_enabled() and self.steps % self.fac_update_freq == 0:
             self.m_a[module] = input[0].data
 
     def _save_grad_output(self, module, grad_input, grad_output):
-        if self.steps % self.cov_update_freq == 0:
+        if self.steps % self.fac_update_freq == 0:
             self.m_g[module] = grad_output[0].data
 
-    def _prepare_model(self):
+    def _register_modules(self):
         for module in self.model.modules():
             classname = module.__class__.__name__
             if classname in self.known_modules:
@@ -78,34 +79,34 @@ class KFAC(optim.Optimizer):
                 module.register_forward_pre_hook(self._save_input)
                 module.register_backward_hook(self._save_grad_output)
 
-    def _init_a_buffers(self, factor, module):
-        self.m_aa[module] = torch.diag(factor.new(factor.shape[0]).fill_(1))
-        self.d_a[module] = factor.new_zeros(factor.shape[0])
-        self.Q_a[module] = factor.new_zeros(factor.shape)
+    def _init_A(self, factor, module):
+        self.m_A[module] = torch.diag(factor.new(factor.shape[0]).fill_(1))
+        self.m_dA[module] = factor.new_zeros(factor.shape[0])
+        self.m_QA[module] = factor.new_zeros(factor.shape)
 
-    def _init_g_buffers(self, factor, module):
-        self.m_gg[module] = torch.diag(factor.new(factor.shape[0]).fill_(1))
-        self.d_g[module] = factor.new_zeros(factor.shape[0])
-        self.Q_g[module] = factor.new_zeros(factor.shape)
+    def _init_G(self, factor, module):
+        self.m_G[module] = torch.diag(factor.new(factor.shape[0]).fill_(1))
+        self.m_dG[module] = factor.new_zeros(factor.shape[0])
+        self.m_QG[module] = factor.new_zeros(factor.shape)
 
-    def _update_cov_a(self):
+    def _update_A(self):
         for module in self.modules: 
-            aa = self.CovAHandler(self.m_a[module], module)
+            a = self.computeA(self.m_a[module], module)
             if self.steps == 0:
-                self._init_a_buffers(aa, module)
-            update_running_stat(aa, self.m_aa[module], self.stat_decay)
+                self._init_A(a, module)
+            update_running_stat(a, self.m_A[module], self.factor_decay)
 
-    def _update_cov_g(self):
+    def _update_G(self):
         for module in self.modules:
-            gg = self.CovGHandler(self.m_g[module], module, self.batch_averaged)
+            g = self.computeG(self.m_g[module], module, self.batch_averaged)
             if self.steps == 0:
-                self._init_g_buffers(gg, module)
-            update_running_stat(gg, self.m_gg[module], self.stat_decay)
+                self._init_G(g, module)
+            update_running_stat(g, self.m_G[module], self.factor_decay)
 
-    def _update_eigen_a(self, m, ranks):
+    def _update_eigen_A(self, module, ranks):
         i = ranks.index(hvd.rank())
         n = len(ranks)
-        a = self.m_aa[m]
+        a = self.m_A[module]
         if n > min(a.shape):
             n = min(a.shape)
 
@@ -114,14 +115,14 @@ class KFAC(optim.Optimizer):
             block = a[start[0]:end[0], start[1]:end[1]]
             d, Q = torch.symeig(block, eigenvectors=True)
             d = torch.mul(d, (d > self.eps).float())
-            self.d_a[m].data[start[0]:end[0]].copy_(d)
-            self.Q_a[m].data[start[0]:end[0], start[1]:end[1]].copy_(Q)
+            self.m_dA[module].data[start[0]:end[0]].copy_(d)
+            self.m_QA[module].data[start[0]:end[0], start[1]:end[1]].copy_(Q)
 
-    def _update_eigen_g(self, m, ranks):
+    def _update_eigen_G(self, module, ranks):
         # Only compute g on first rank to enter this function
         i = ranks.index(hvd.rank())
         n = len(ranks)
-        g = self.m_gg[m]
+        g = self.m_G[module]
         if n > min(g.shape):
             n = min(g.shape)
 
@@ -130,11 +131,10 @@ class KFAC(optim.Optimizer):
             block = g[start[0]:end[0], start[1]:end[1]]
             d, Q = torch.symeig(block, eigenvectors=True)
             d = torch.mul(d, (d > self.eps).float())
-            self.d_g[m].data[start[0]:end[0]].copy_(d)
-            self.Q_g[m].data[start[0]:end[0], start[1]:end[1]].copy_(Q)
+            self.m_dG[module].data[start[0]:end[0]].copy_(d)
+            self.m_QG[module].data[start[0]:end[0], start[1]:end[1]].copy_(Q)
 
-    @staticmethod
-    def _get_matrix_form_grad(m, classname):
+    def _get_grad(self, module, classname):
         """
         :param m: the layer
         :param classname: the class name of the layer
@@ -142,58 +142,59 @@ class KFAC(optim.Optimizer):
         """
         if classname == 'Conv2d':
             # n_filters * (in_c * kw * kh)
-            p_grad_mat = m.weight.grad.data.view(m.weight.grad.data.size(0), -1)  
+            grad = module.weight.grad.data.view(module.weight.grad.data.size(0), -1)  
         else:
-            p_grad_mat = m.weight.grad.data
+            grad = module.weight.grad.data
         if m.bias is not None:
-            p_grad_mat = torch.cat([p_grad_mat, m.bias.grad.data.view(-1, 1)], 1)
-        return p_grad_mat
+            grad = torch.cat([grad, module.bias.grad.data.view(-1, 1)], 1)
+        return grad
 
-    def _get_natural_grad(self, m, p_grad_mat):
+    def _get_preconditioned_grad(self, module, grad):
         """
         :param m:  the layer
         :param p_grad_mat: the gradients in matrix form
         :return: a list of gradients w.r.t to the parameters in `m`
         """
-        v1 = self.Q_g[m].t() @ p_grad_mat @ self.Q_a[m]
-        v2 = v1 / (self.d_g[m].unsqueeze(1) * self.d_a[m].unsqueeze(0) + 
+        v1 = self.m_QG[module].t() @ grad @ self.m_QA[module]
+        v2 = v1 / (self.m_dG[module].unsqueeze(1) * self.m_dA[module].unsqueeze(0) + 
                    self.damping)
-        v = self.Q_g[m] @ v2 @ self.Q_a[m].t()
+        v = self.m_QG[module] @ v2 @ self.m_QA[module].t()
 
-        if m.bias is not None:
+        if module.bias is not None:
             # we always put gradient w.r.t weight in [0]
             # and w.r.t bias in [1]
             v = [v[:, :-1], v[:, -1:]]
-            v[0] = v[0].view(m.weight.grad.data.size())
-            v[1] = v[1].view(m.bias.grad.data.size())
+            v[0] = v[0].view(module.weight.grad.data.size())
+            v[1] = v[1].view(module.bias.grad.data.size())
         else:
-            v = [v.view(m.weight.grad.data.size())]
+            v = [v.view(module.weight.grad.data.size())]
         return v
 
-    def _kl_clip_and_update_grad(self, updates):
+    def _update_scale_grad(self, updates):
         # do kl clip
         vg_sum = 0
-        for m in self.modules:
-            v = updates[m]
-            vg_sum += (v[0] * m.weight.grad.data * self.lr ** 2).sum().item()
-            if m.bias is not None:
-                vg_sum += (v[1] * m.bias.grad.data * self.lr ** 2).sum().item()
+        for module in self.modules:
+            v = updates[module]
+            vg_sum += (v[0] * module.weight.grad.data * self.lr ** 2).sum().item()
+            if module.bias is not None:
+                vg_sum += (v[1] * module.bias.grad.data * self.lr ** 2).sum().item()
         nu = min(1.0, math.sqrt(self.kl_clip / abs(vg_sum)))
 
-        for m in self.modules:
-            v = updates[m]
-            m.weight.grad.data.copy_(v[0])
-            m.weight.grad.data.mul_(nu)
-            if m.bias is not None:
-                m.bias.grad.data.copy_(v[1])
-                m.bias.grad.data.mul_(nu)
+        for module in self.modules:
+            v = updates[module]
+            module.weight.grad.data.copy_(v[0])
+            module.weight.grad.data.mul_(nu)
+            if module.bias is not None:
+                module.bias.grad.data.copy_(v[1])
+                module.bias.grad.data.mul_(nu)
 
     def step(self, closure=None, epoch=None):
         group = self.param_groups[0]
         self.lr = group['lr']
         self.damping = group['damping']
-        self.cov_update_freq = group['cov_update_freq']
+        self.fac_update_freq = group['fac_update_freq']
         self.inv_update_freq = group['inv_update_freq']
+
         updates = {}
         handles = []
 
@@ -205,11 +206,11 @@ class KFAC(optim.Optimizer):
         else:
             diag_blocks = self.diag_blocks if epoch >= self.diag_warmup else 1
 
-        if self.steps % self.cov_update_freq == 0:
-            self._update_cov_a()
-            self._update_cov_g()
+        if self.steps % self.fac_update_freq == 0:
+            self._update_A()
+            self._update_G()
             if hvd.size() > 1:
-                self.allreduce_covs()
+                self._allreduce_factors()
 
         # if we are switching from no approx to approx, we need to clear
         # off-block-diagonal elements
@@ -217,16 +218,16 @@ class KFAC(optim.Optimizer):
                 epoch == self.diag_warmup and \
                 self.steps % self.inv_update_freq == 0:
             for m in self.modules:
-                self.Q_a[m].fill_(0)
-                self.Q_g[m].fill_(0)
+                self.m_QA[m].fill_(0)
+                self.m_QG[m].fill_(0)
             self.have_cleared_Q = True
 
         if self.steps % self.inv_update_freq == 0:
             # reset rank iter so device get the same layers
             # to compute to take advantage of caching
             self.rank_iter.reset() 
-            for m in self.modules:
-                n = diag_blocks if m.__class__.__name__ == 'Conv2d' else 1
+            for module in self.modules:
+                n = diag_blocks if module.__class__.__name__ == 'Conv2d' else 1
                 ranks_a = self.rank_iter.next(n)
                 if self.distribute_layer_factors:
                     ranks_g = self.rank_iter.next(n)
@@ -234,48 +235,48 @@ class KFAC(optim.Optimizer):
                     ranks_g = ranks_a
 
                 if hvd.rank() in ranks_a:
-                    self._update_eigen_a(m, ranks_a)
+                    self._update_eigen_a(module, ranks_a)
                 else:
-                    self.Q_a[m].fill_(0)
-                    self.d_a[m].fill_(0)
+                    self.m_QA[module].fill_(0)
+                    self.m_dA[module].fill_(0)
 
                 if hvd.rank() in ranks_g:
-                    self._update_eigen_g(m, ranks_g)
+                    self._update_eigen_g(module, ranks_g)
                 else:
-                    self.Q_g[m].fill_(0)
-                    self.d_g[m].fill_(0)
+                    self.m_QG[module].fill_(0)
+                    self.m_dG[module].fill_(0)
 
             if hvd.size() > 1:
-                self.allreduce_eigens()
+                self._allreduce_eigendecomp()
 
-        for m in self.modules:
-            classname = m.__class__.__name__
-            p_grad_mat = self._get_matrix_form_grad(m, classname)
-            v = self._get_natural_grad(m, p_grad_mat)
-            updates[m] = v
+        for module in self.modules:
+            classname = module.__class__.__name__
+            grad = self._get_grad(module, classname)
+            precon_grad = self._get_preconditioned_grad(module, grad)
+            updates[module] = precon_grad
 
-        self._kl_clip_and_update_grad(updates)
+        self._update_scale_grad(updates)
 
         self.steps += 1
 
-    def allreduce_covs(self):
+    def _allreduce_factors(self):
         handles = []
 
         for m in self.modules:
-            handles.append(hvd.allreduce_async_(self.m_aa[m].data, op=hvd.Average))
-            handles.append(hvd.allreduce_async_(self.m_gg[m].data, op=hvd.Average))
+            handles.append(hvd.allreduce_async_(self.m_A[m].data, op=hvd.Average))
+            handles.append(hvd.allreduce_async_(self.m_G[m].data, op=hvd.Average))
 
         for handle in handles:
             hvd.synchronize(handle)
 
-    def allreduce_eigens(self):
+    def _allreduce_eigendecomp(self):
         handles = []
 
         for m in self.modules:
-            handles.append(hvd.allreduce_async_(self.Q_a[m].data, op=hvd.Sum))
-            handles.append(hvd.allreduce_async_(self.Q_g[m].data, op=hvd.Sum))
-            handles.append(hvd.allreduce_async_(self.d_a[m].data, op=hvd.Sum))
-            handles.append(hvd.allreduce_async_(self.d_g[m].data, op=hvd.Sum))
+            handles.append(hvd.allreduce_async_(self.m_QA[m].data, op=hvd.Sum))
+            handles.append(hvd.allreduce_async_(self.m_QG[m].data, op=hvd.Sum))
+            handles.append(hvd.allreduce_async_(self.m_dA[m].data, op=hvd.Sum))
+            handles.append(hvd.allreduce_async_(self.m_dG[m].data, op=hvd.Sum))
     
         for handle in handles:
             hvd.synchronize(handle)
@@ -286,7 +287,7 @@ class KFACParamScheduler():
                  kfac,
                  damping_alpha=1,
                  damping_schedule=None,
-                 update_freq_alpha=1,
+                 update_frem_QAlpha=1,
                  update_freq_schedule=None,
                  start_epoch=0):
 
@@ -300,13 +301,13 @@ class KFACParamScheduler():
                 self.get_factor_func(self.damping_schedule,
                                      self.damping_alpha)
 
-        self.cov_update_freq_base = params['cov_update_freq']
+        self.fac_update_freq_base = params['fac_update_freq']
         self.inv_update_freq_base = params['inv_update_freq']
-        self.update_freq_alpha = update_freq_alpha
+        self.update_frem_QAlpha = update_frem_QAlpha
         self.update_freq_schedule = update_freq_schedule
         self.update_freq_factor_func = \
                 self.get_factor_func(self.update_freq_schedule,
-                                     self.update_freq_alpha)
+                                     self.update_frem_QAlpha)
 
         self.epoch = start_epoch
 
@@ -336,5 +337,5 @@ class KFACParamScheduler():
         params['damping'] = self.damping_base * self.damping_factor_func(self.epoch)
 
         factor = self.update_freq_factor_func(self.epoch)
-        params['cov_update_freq'] = int(self.cov_update_freq_base * factor)
+        params['fac_update_freq'] = int(self.fac_update_freq_base * factor)
         params['inv_update_freq'] = int(self.inv_update_freq_base * factor)
