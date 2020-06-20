21841c3f8d4ac296bf0eda0e34a8502585277673
Greg Pauloski <gpauloski@yahoo.com>
2020-06-20 12:08:19 -0500

    Refactored layer registering
    Former-commit-id: 07d6c2c9503da0daae8434aa95e9d4f36a382174

--- kfac/layers/__init__.py
+++ kfac/layers/__init__.py
@@ -1,13 +1,26 @@
 import torch.nn as nn
 
+import kfac.modules as km
 from kfac.layers.conv import Conv2dLayer
 from kfac.layers.embedding import EmbeddingLayer
 from kfac.layers.linear import LinearLayer
 from kfac.layers.rnn import RNNLayer
 
-__all__ = ['get_kfac_layer', 'KNOWN_MODULES', 'module_requires_grad']
+__all__ = ['get_kfac_layer', 'KFAC_LAYERS', 'KNOWN_MODULES',
+           'module_requires_grad']
 
-KNOWN_MODULES = {'linear', 'conv2d', 'embedding'}
+# PyTorch modules that have KFACLayer implementations.
+KFAC_LAYERS = {'linear', 'conv2d', 'embedding'}
+
+# PyTorch/KFAC parent modules that can be registered using KFACLayers.
+# This is different from `KFAC_LAYERS` because some modules (e.g. 
+# kfac.modules.LSTMCell) are made up of PyTorch modules in `KFAC_Layer`
+# that we want to register even if the user has decided to manually skip
+# that type of module. E.g. a user may want to skip Linear layers but
+# register RNNCells; however, RNNCells is made up of Linear layers that we
+# do not want to skip, hence this separation of `KFAC_LAYERS` and 
+# `KNOWN_MODULES`.
+KNOWN_MODULES = {'linear', 'conv2d', 'embedding', 'LSTMCell', 'RNNCell'}
 
 def get_kfac_layer(module, use_eigen_decomp=True, damping=0.001,
                    factor_decay=0.95, batch_averaged=True):
@@ -15,10 +28,13 @@ def get_kfac_layer(module, use_eigen_decomp=True, damping=0.001,
         layer = LinearLayer
     elif isinstance(module, nn.Conv2d):
         layer = Conv2dLayer
-    elif isinstance(module, nn.RNNCellBase):
-        layer = RNNLayer
+    #elif isinstance(module, km.RNNCell) or isinstance(module, km.LSTMCell):
+    #    layer = RNNLayer
     elif isinstance(module, nn.Embedding):
         layer = EmbeddingLayer
+    elif isinstance(module, nn.RNNCellBase):
+        raise TypeError('KFAC does not support torch.nn.{RNN,LSTM}Cell. Use '
+                        'kfac.modules.{RNN,LSTM}Cell instead for KFAC support.')
     else:
         raise NotImplementedError('KFAC does not support layer {}'.format(
                                   module.__class__.__name__))
