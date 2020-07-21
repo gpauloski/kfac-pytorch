4c1babd55724245e8074d84fd9064ddce47b4607
Greg Pauloski <gpauloski@yahoo.com>
2020-07-21 15:59:56 -0500

    Replace torch.stack with in-place sum to save memory when computing RNN factors
    Former-commit-id: 8fb1c794d68f2fbea8f6aa9bf318b1bae544214c

--- kfac/layers/linear_rnn.py
+++ kfac/layers/linear_rnn.py
@@ -25,7 +25,7 @@ class LinearRNNLayer(KFACLayer):
 
     def _compute_A_factor(self):
         # a: batch_size * in_dim
-        a_o = []
+        a_o = None
         for t in range(len(self.a_inputs)):
             a_t = self.a_inputs[t]
             batch_size = a_t.size(0)
@@ -33,17 +33,19 @@ class LinearRNNLayer(KFACLayer):
             if self.has_bias:
                 a_t = torch.cat([a_t, a_t.new(a_t.size(0), 1).fill_(1)], 1)
             a_t = a_t.t() @ (a_t / batch_size)
-            a_o.append(a_t)
 
-        a_o = torch.stack(a_o)
-        a_o = torch.mean(a_o, dim=0)
+            if a_o is None:
+                a_o = a_t
+            else:
+                a_o += a_t
 
         self.a_inputs = []  # Clear input accumulation
+
         return a_o
 
     def _compute_G_factor(self): 
         # g: batch_size * out_dim
-        g_o = []
+        g_o = None
         for t in range(len(self.g_outputs)):
             g_t = self.g_outputs[t]
             batch_size = g_t.size(0)
@@ -52,12 +54,14 @@ class LinearRNNLayer(KFACLayer):
                 g_t = g_t.t() @ (g_t * batch_size)
             else:
                 g_t = g_t.t() @ (g_t / batch_size)
-            g_o.append(g_t)
 
-        g_o = torch.stack(g_o)
-        g_o = torch.mean(g_o, dim=0)
+            if g_o is None:
+                g_o = g_t
+            else:
+                g_o += g_t
 
         self.g_outputs = []  # Clear grad_output accumulation now that we have
                              # computed G for these grad_outputs
+
         return g_o
 
