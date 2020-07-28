aeeb06f7e580bcdbf4d82acf6cfc67430a3c1d46
Greg Pauloski <gpauloski@yahoo.com>
2020-07-27 23:57:57 -0500

    LSTM example now reaches correct baseline with SGD
    Former-commit-id: 9bde9eedebdf09d707fb7ea531fc95ddbf41a3bf

--- examples/rnn_utils/lstm.py
+++ examples/rnn_utils/lstm.py
@@ -9,19 +9,24 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-import kfac
+from kfac.modules import LSTM
 
 class LSTMModel(nn.Module):
     """Container module with an encoder, a recurrent module, and a decoder."""
 
     def __init__(self, ntoken, ninp, nhid, nlayers, dropout=0.5, tie_weights=False, 
-                 batch_first=False):
+                 batch_first=False, init_weight=0.05):
         super(LSTMModel, self).__init__()
+        self.nhid = nhid
+        self.nlayers = nlayers
         self.ntoken = ntoken
         self.batch_first = batch_first
         self.drop = nn.Dropout(dropout)
-        self.encoder = nn.Embedding(ntoken, ninp) 
-        self.lstm = kfac.modules.LSTM(ninp, nhid, nlayers, dropout=dropout, batch_first=batch_first)
+        self.encoder = nn.Embedding(ntoken, ninp)
+        self.layers = nn.ModuleList([
+            LSTM(ninp, nhid, dropout=dropout, batch_first=batch_first)
+            for i in range(nlayers)
+        ])
         self.decoder = nn.Linear(nhid, ntoken)
 
         # Optionally tie weights as in:
@@ -35,28 +40,24 @@ class LSTMModel(nn.Module):
                 raise ValueError('When using the tied flag, nhid must be equal to emsize')
             self.decoder.weight = self.encoder.weight
 
-        self.init_weights()
+        for param in self.parameters():
+            nn.init.uniform_(param, -init_weight, init_weight)
 
-        self.nhid = nhid
-        self.nlayers = nlayers
-
-    def init_weights(self):
-        initrange = 0.1
-        self.encoder.weight.data.uniform_(-initrange, initrange)
-        self.decoder.bias.data.zero_()
-        self.decoder.weight.data.uniform_(-initrange, initrange)
+    def detach(self, hidden):
+        return [(h.detach(), c.detach()) for (h, c) in hidden]
 
     def forward(self, input, hidden=None):
         if hidden is None:
             hidden = self.init_hidden(input.size(0 if self.batch_first else 1))
-        emb = self.drop(self.encoder(input))
-        output, hidden = self.lstm(emb, hidden)
-        output = self.drop(output)
-        decoded = self.decoder(output)
+        x = self.drop(self.encoder(input))
+        for i, layer in enumerate(self.layers):
+            x, hidden[i] = layer(x, hidden[i])
+            x = self.drop(x)
+        decoded = self.decoder(x)
         decoded = decoded.view(-1, self.ntoken)
         return F.log_softmax(decoded, dim=1), hidden
 
     def init_hidden(self, bsz):
-        weight = next(self.parameters())
-        return (weight.new_zeros(self.nlayers, bsz, self.nhid),
-                weight.new_zeros(self.nlayers, bsz, self.nhid))
+        w = next(self.parameters())
+        return [(w.new_zeros(1, bsz, self.nhid), w.new_zeros(1, bsz, self.nhid))
+                for i in range(self.nlayers)]
