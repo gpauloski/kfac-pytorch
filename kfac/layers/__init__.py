4709fdf59df695db7c4a4a7d63f2e88d1ceef171
Greg Pauloski <gpauloski@yahoo.com>
2020-07-29 14:31:06 -0500

    Refactored Linear Layer variants
    - Removed linear_rnn_opt2 in reference to previous commit.
    - Renamed LinearRNNLayer to LinearMultiLayer to better reflect the class is for Linear modules that get called multiple times per training item. E.g. an LSTMCell has its .forward() called once per timestep in a sequence.
    - Moved LinearMultiLayer to kfac/layers/linear.py
    
    
    Former-commit-id: 8010b857e409be829a89d3b07b4e1d37a5417a8f

--- kfac/layers/__init__.py
+++ kfac/layers/__init__.py
@@ -4,7 +4,7 @@ import kfac.modules as km
 from kfac.layers.conv import Conv2dLayer
 from kfac.layers.embedding import EmbeddingLayer
 from kfac.layers.linear import LinearLayer
-from kfac.layers.linear_rnn import LinearRNNLayer
+from kfac.layers.linear import LinearMultiLayer
 
 __all__ = ['KNOWN_MODULES', 'get_kfac_layers', 'module_requires_grad']
 
@@ -27,7 +27,7 @@ def get_kfac_layers(module, **kwargs):
     elif isinstance(module, nn.Embedding):
         return [(module, EmbeddingLayer(module, **kwargs))]
     elif isinstance(module, km.LSTMCell):
-        return [(m, LinearRNNLayer(m, **kwargs)) for m in module.children()]
+        return [(m, LinearMultiLayer(m, **kwargs)) for m in module.children()]
     elif isinstance(module, nn.RNNCellBase):
         raise TypeError('KFAC does not support torch.nn.{RNN,LSTM}Cell. Use '
                         'kfac.modules.{RNN,LSTM}Cell instead for KFAC support.')
