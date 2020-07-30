9513ac3278e27de92b7dd24f3ca870e70f495cfd
Greg Pauloski <gpauloski@yahoo.com>
2020-07-30 15:55:16 -0500

    Added input/output accumulation
    - When a module is called multiple times during one step, the input and gradient w.r.t output are accumulated as a list. This is most common for RNNCells that get called once per timestep for each batch. This commit extend this functionality from kfac.layers.LinearMultiLayer to all kfac.layers.
    - Added batch_first parameter to KFAC(). This is needed to know how to concatenate the accumulated inputs and outputs. get_optimizer() in the examples is extended to support this.
    - Exposed KFAC module registration classes so users can manually register modules.
    - Created a KFAC module registration function for modules that share weights with another module (e.g. embeddings weights tied to a linear decoder in language modeling). This custom registration function is needed to properly register hooks for both torch modules to the same KFAC layer.
    - Running average of factors is now initialized to the first factor that is calculated instead of the identity matrix.
    - WIP: updating KFAC support for embedding layers. Now that tied embeddings are support, the KFAC EmbeddingLayer is being rewritten to support contributions from the embedding module itself but also the linear module that shares it weight.

--- kfac/preconditioner.py
+++ kfac/preconditioner.py
@@ -43,6 +43,8 @@ class KFAC(optim.Optimizer):
           the KFAC update, otherwise use normal inv method (default: True)
       batch_averaged (bool, optional): boolean representing if the gradient
           is alrady averaged across the batches (default: True)
+      batch_first (bool, optional): True if the batch dimension is dim 0
+          (default: True)
       distribute_layer_factors (bool, optional): if `True`, computes factors A
           and G on different workers else computes A and G for a single layer
           on the same worker. For small worker counts, computing per layer
@@ -61,6 +63,7 @@ class KFAC(optim.Optimizer):
                  kfac_update_freq=100,
                  use_eigen_decomp=True,
                  batch_averaged=True,
+                 batch_first=True,
                  distribute_layer_factors=True,
                  skip_layers=None,
                  verbose=True):
@@ -100,6 +103,7 @@ class KFAC(optim.Optimizer):
         self.kfac_update_freq = kfac_update_freq
         self.use_eigen_decomp = use_eigen_decomp
         self.batch_averaged = batch_averaged
+        self.batch_first = batch_first
         self.distribute_layer_factors = distribute_layer_factors
         self.verbose = verbose
 
@@ -112,21 +116,11 @@ class KFAC(optim.Optimizer):
             for layer in skip_layers: 
                 self.known_modules.discard(layer.lower())
 
-        self.layers = {}  # key: nn.Module, value: KFACLayer
-        self._register_modules(model)
+        self.layers = []
+        self.hook_layers = {}  # key: nn.Module, value: KFACLayer
+        self.register_modules(model)
 
-    def _save_input(self, module, input):
-        """Hook for saving layer input"""
-        if (torch.is_grad_enabled() and 
-                self.steps % self.fac_update_freq == 0):
-            self.layers[module].save_inputs(input)
-
-    def _save_grad_output(self, module, grad_input, grad_output):
-        """Hook for saving gradient w.r.t output"""
-        if self.steps % self.fac_update_freq == 0:
-            self.layers[module].save_grad_outputs(grad_output)
-
-    def _register_module(self, module):
+    def register_module(self, module):
         """Create and register a KFAC layer for a module.
 
         Note: For a single module, there may be multiple KFAC layers
@@ -139,39 +133,77 @@ class KFAC(optim.Optimizer):
             use_eigen_decomp = self.use_eigen_decomp, 
             damping = self.damping,
             factor_decay = self.factor_decay,
-            batch_averaged = self.batch_averaged
+            batch_averaged = self.batch_averaged,
+            batch_first = self.batch_first
         )
         for module, kfac_layer in layer_list:
             if self.backend.rank() == 0 and self.verbose:
                 print('Registered: {}'.format(kfac_layer))
-            self.layers[module] = kfac_layer
+            self.hook_layers[module] = kfac_layer
+            self.layers.append(kfac_layer)
             module.register_forward_pre_hook(self._save_input)
             module.register_backward_hook(self._save_grad_output)
 
-    def _register_modules(self, model):
+    def register_modules(self, model):
         """Iterate over and register modules that KFAC supports."""
         for module in model.children():
             if module.__class__.__name__.lower() not in self.known_modules:
-                self._register_modules(module)
+                self.register_modules(module)
             else:
                 if kfac_layers.module_requires_grad(module):
-                    self._register_module(module)
+                    self.register_module(module)
 
-    def _compute_grad_scale(self):
-        """Computes scale factor for preconditioned gradients
+    def register_shared_module(self, main_module, second_module, reverse_hooks=False):
+        """Create and register a KFAC layer for modules that share a weight
 
-        Returns:
-          sum_{layers} (sum_{gradients} precon_grad * grad * lr^2) 
+        Useful for the case where two modules share a weight matrix and you want to
+        incorporate the input and grad_output for both modules. E.g. in a language
+        model it is common to tie the embedding and decoding (a linear module) weights
+        but if only the embedding module is registered with KFAC, the forward and
+        backward pass information will be lost for the linear module.
+
+        Args:
+          main_module (nn.Module): main module to register, a pointer to this module
+              will be saved with the KFACLayer instance.
+          second_module (nn.Module): the secondary module that shares its weight matrix
+              with `main_module`. Only the forward/backward hooks will be registered
+              for this module.
+          reverse_hooks (bool, optional): if True, reverse the hooks for the
+              `second_module`. Useful in cases such as tied embeddings where the input
+              to the embedding is related to the output of the decoding.
         """
-        vg_sum = 0.
-        for module, layer in self.layers.items():
-            v = layer.preconditioned_gradient
-            vg_sum += (v[0] * layer._get_weight_grad().data *
-                       self.lr ** 2).sum().item()
-            if layer.has_bias:
-                vg_sum += (v[1] * layer._get_bias_grad().data * 
-                           self.lr ** 2).sum().item()
-        return min(1.0, math.sqrt(self.kl_clip / abs(vg_sum)))
+        if not isinstance(main_module, torch.nn.Module):
+            raise ValueError('main_module must be of type torch.nn.Module')
+        if not isinstance(second_module, torch.nn.Module):
+            raise ValueError('second_module must be of type torch.nn.Module')
+        layer_list = kfac_layers.get_kfac_layers(
+            main_module,
+            use_eigen_decomp = self.use_eigen_decomp, 
+            damping = self.damping,
+            factor_decay = self.factor_decay,
+            batch_averaged = self.batch_averaged,
+            batch_first = self.batch_first
+        )
+        
+        if len(layer_list) > 1:
+            raise ValueError('KFAC registering for shared weight modules does not work '
+                             'for modules with multiple KFACLayers (e.g. LSTMCells)')
+        else:
+            _, kfac_layer = layer_list[0]
+
+        if self.backend.rank() == 0 and self.verbose:
+            print('Registered: {} (shared weight)'.format(kfac_layer))
+        self.hook_layers[main_module] = kfac_layer
+        self.hook_layers[second_module] = kfac_layer
+        self.layers.append(kfac_layer)
+        main_module.register_forward_pre_hook(self._save_input)
+        main_module.register_backward_hook(self._save_grad_output)
+        if reverse_hooks:
+            second_module.register_forward_pre_hook(self._save_input_as_grad_output)
+            second_module.register_backward_hook(self._save_grad_output_as_input)
+        else:
+            second_module.register_forward_pre_hook(self._save_input)
+            second_module.register_backward_hook(self._save_grad_output)
 
     @torch.no_grad()
     def step(self, closure=None):
@@ -197,7 +229,7 @@ class KFAC(optim.Optimizer):
         handles = []
 
         if self.steps % self.fac_update_freq == 0:
-            for layer in self.layers.values():
+            for layer in self.layers:
                 layer.update_A_factor()
                 layer.update_G_factor()
 
@@ -212,18 +244,18 @@ class KFAC(optim.Optimizer):
 
         if self.steps % self.kfac_update_freq == 0:
             rank = self.backend.rank()
-            for i, layer in enumerate(self.layers.values()):
+            for i, layer in enumerate(self.layers):
                 layer.compute_A_inv(rank)
                 layer.compute_G_inv(rank)
             if self.backend.size() > 1:
                 self._broadcast_inverses()
 
-        for layer in self.layers.values():
+        for layer in self.layers:
             layer.compute_preconditioned_gradient()
 
         nu = None if self.kl_clip is None else self._compute_grad_scale()
 
-        for layer in self.layers.values():
+        for layer in self.layers:
             layer.update_gradient(nu)
 
         self.steps += 1
@@ -232,23 +264,10 @@ class KFAC(optim.Optimizer):
         """Allreduce the factors for all layers"""
         tensors = []
 
-        for layer in self.layers.values():
+        for layer in self.layers:
             tensors.extend(layer.get_factors())
 
         self.backend.allreduce(tensors, op=self.backend.Average)
-
-    def _broadcast_inverses(self):
-        """Broadcast the eigendecomp/invs for all layers"""
-        tensors = []
-        ranks = []
-
-        for layer in self.layers.values():
-            tensor_list, rank_list = layer.get_inverses(return_ranks=True)
-            tensors.extend(tensor_list)
-            ranks.extend(rank_list)
-
-        assert len(tensors) == len(ranks) 
-        self.backend.broadcast(tensors, ranks)
 
     def _assign_layers_to_workers(self):
         """Assigns layers to workers to minimize max load on any worker.
@@ -260,8 +279,8 @@ class KFAC(optim.Optimizer):
             return
 
         func = lambda n: n**3  # approx inverse complexity
-        a_sizes = [l.A_inv.shape[0] for l in self.layers.values()]
-        g_sizes = [l.G_inv.shape[0] for l in self.layers.values()]
+        a_sizes = [l.A_inv.shape[0] for l in self.layers]
+        g_sizes = [l.G_inv.shape[0] for l in self.layers]
         a_times = list(map(func, a_sizes))
         g_times = list(map(func, g_sizes))
             
@@ -274,6 +293,64 @@ class KFAC(optim.Optimizer):
             locs = utils.load_balance(self.backend.size(), times)
             a_locs, g_locs = locs, locs
 
-        for i, layer in enumerate(self.layers.values()):
+        for i, layer in enumerate(self.layers):
             layer.A_rank = a_locs[i]
             layer.G_rank = g_locs[i]
+
+    def _broadcast_inverses(self):
+        """Broadcast the eigendecomp/invs for all layers"""
+        tensors = []
+        ranks = []
+
+        for layer in self.layers:
+            tensor_list, rank_list = layer.get_inverses(return_ranks=True)
+            tensors.extend(tensor_list)
+            ranks.extend(rank_list)
+
+        assert len(tensors) == len(ranks) 
+        self.backend.broadcast(tensors, ranks)
+
+    def _compute_grad_scale(self):
+        """Computes scale factor for preconditioned gradients
+
+        Returns:
+          sum_{layers} (sum_{gradients} precon_grad * grad * lr^2) 
+        """
+        vg_sum = 0.
+        for layer in self.layers:
+            v = layer.preconditioned_gradient
+            vg_sum += (v[0] * layer._get_weight_grad().data *
+                       self.lr ** 2).sum().item()
+            if layer.has_bias:
+                vg_sum += (v[1] * layer._get_bias_grad().data * 
+                           self.lr ** 2).sum().item()
+        return min(1.0, math.sqrt(self.kl_clip / abs(vg_sum)))
+
+    def _periodic_hook(grad_enabled=True):
+        def decorator(func):
+            def wrapper(self, *args, **kwargs):
+                if grad_enabled:
+                    if (torch.is_grad_enabled() and 
+                        self.steps % self.fac_update_freq == 0):
+                        func(self, *args, **kwargs)
+                else:
+                    if self.steps % self.fac_update_freq == 0:
+                        func(self, *args, **kwargs)
+            return wrapper
+        return decorator
+
+    @_periodic_hook(grad_enabled=True)
+    def _save_input(self, module, input):
+        self.hook_layers[module].save_inputs(input)
+
+    @_periodic_hook(grad_enabled=False)
+    def _save_grad_output(self, module, grad_input, grad_output):
+        self.hook_layers[module].save_grad_outputs(grad_output)
+
+    @_periodic_hook(grad_enabled=True)
+    def _save_input_as_grad_output(self, module, input):
+        self.hook_layers[module].save_grad_outputs(input)
+
+    @_periodic_hook(grad_enabled=False)
+    def _save_grad_output_as_input(self, module, grad_input, grad_output):
+        self.hook_layers[module].save_inputs(grad_output)
