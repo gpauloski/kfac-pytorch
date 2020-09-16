67a291a4f95a49e79a07d7986f81492c6fc7c06c
Greg Pauloski <gpauloski@yahoo.com>
2020-09-16 17:04:49 -0500

    Update imagenet training scripts
    Former-commit-id: f087bd394dea1cbdfcfcfb6748ab3c4d6841319d

--- sbatch/longhorn/launch_node_torch_imagenet.sh
+++ sbatch/longhorn/launch_node_torch_imagenet.sh
@@ -1,48 +1,72 @@
 #!/bin/bash
 
-NGPUS=4
+NGPUS=1
 NNODES=1
 MASTER=""
+MVAPICH=false
 
-while getopts ":hcn:N:m:b:i:s:l:" opt; do
-  case $opt in
-    h) echo "-h         Display this help message"
-       echo "-n [ngpus] Number of GPUs to use on each node (default 2)"
-       echo "-N [nodes] Number of nodes to use (default 1)"
-       echo "-m [mastr] Address of master node. Only used if NNODES>1"
-       exit 0
-    ;;
-    n) NGPUS="$OPTARG"
-    ;;
-    N) NNODES="$OPTARG"
-    ;;
-    m) MASTER="$OPTARG"
-    ;;
-    \?) echo "Invalid option -$OPTARG" >&2
-  esac
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | sed 's/^[^=]*=//g'`
+    if [[ "$VALUE" == "$PARAM" ]]; then
+        shift
+        VALUE=$1
+    fi
+    case $PARAM in
+        -h|--help)
+            echo "USAGE: ./launch_node_torch_imagenet.sh"
+            echo "  -h,--help           Display this help message"
+            echo "  -N,--ngpus [count]  Number of GPUs per node (default: 1)"
+            echo "  -n,--nnodes [count] Number of nodes this script is launched on (default: 1)"
+            echo "  -m,--master [addr]  Address of master node (default: \"\")"
+            echo "  --mvapich           Use MVAPICH env variables for initialization (default: false)"
+            exit 0
+        ;;
+        -N|--ngpus)
+            NGPUS=$VALUE
+        ;;
+        -n|--nnodes)
+            NNODES=$VALUE
+        ;;
+        -m|--master)
+            MASTER=$VALUE
+        ;;      
+        --mvapich)
+            MVAPICH=true
+        ;;
+        *)
+          echo "ERROR: unknown parameter \"$PARAM\""
+          exit 1
+        ;;
+    esac
+    shift
 done
+
+if [ "$MVAPICH" == true ]; then
+  LOCAL_RANK=$MV2_COMM_WORLD_RANK
+else
+  LOCAL_RANK=$OMPI_COMM_WORLD_RANK
+fi
+
+echo Launching torch.distributed: nproc_per_node=$NGPUS, nnodes=$NNODES, master_addr=$MASTER, local_rank=$LOCAL_RANK, using_mvapich=$MVAPICH
+
+KWARGS=""
+KWARGS+="--kfac-update-freq 100 "
+KWARGS+="--kfac-cov-update-freq 10 "
+KWARGS+="--damping 0.001 "
+KWARGS+="--epochs 55 "
+KWARGS+="--lr-decay 25 35 40 45 50 "
+KWARGS+="--model resnet50 "
 
 if [ $NNODES -eq 1 ]; then
   python -m torch.distributed.launch \
       --nproc_per_node=$NGPUS \
-    examples/torch_imagenet_resnet.py \
-      --kfac-update-freq 100 \
-      --kfac-cov-update-freq 10 \
-      --damping 0.001 \
-      --epochs 55 \
-      --lr-decay 25 35 40 45 50 \
-      --model resnet50
+    examples/torch_imagenet_resnet.py $KWARGS
 else
   python -m torch.distributed.launch \
       --nproc_per_node=$NGPUS \
       --nnodes=$NNODES \
-      --node_rank=$OMPI_COMM_WORLD_RANK \
-      --master_addr="$MASTER" \
-    examples/torch_imagenet_resnet.py \
-      --kfac-update-freq 100 \
-      --kfac-cov-update-freq 10 \
-      --damping 0.001 \
-      --epochs 55 \
-      --lr-decay 25 35 40 45 50 \
-      --model resnet50
+      --node_rank=$LOCAL_RANK \
+      --master_addr=$MASTER \
+    examples/torch_imagenet_resnet.py $KWARGS
 fi
