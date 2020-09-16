b7bf60db3c7eb03d893f976ab68d684150198bbd
Greg Pauloski <gpauloski@yahoo.com>
2020-09-16 17:04:49 -0500

    Update imagenet training scripts
    Former-commit-id: 42d9f7966e673b2f6a6ec7a9e44ed2d7cbe1d092

--- sbatch/cp_imagenet_to_temp.sh
+++ sbatch/cp_imagenet_to_temp.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-echo "Copying imagenet.tar to /tmp/"
-cp /scratch/05714/jgpaul/imagenet.tar /tmp/
+# Copy imagenet tar file to /tmp/ and extract.
+# Use --tiny to copy a smaller version of imagenet instead.
+# Produces directories: /tmp/imagenet/ILSVRC2012_img_{train,val}
+
+tiny=${tiny:-false}
+
+while [ $# -gt 0 ]; do
+  if [[ $1 == *"--"* ]]; then
+      param="${1/--/}"
+      declare $param=true
+  fi
+  shift
+done
+
+DEST=/tmp
+if [ "$tiny" == true ]; then
+  SOURCE=/scratch/05714/jgpaul/datasets/imagenet-tiny.tar
+else
+  SOURCE=/scratch/05714/jgpaul/datasets/imagenet.tar
+fi
+
+echo Copying $SOURCE to $DEST
+cp $SOURCE $DEST/imagenet.tar
 echo "Done copying. Extracting"
-tar xf /tmp/imagenet.tar -C /tmp/
+mkdir $DEST/imagenet
+tar xf $DEST/imagenet.tar -C $DEST/imagenet --strip-components 1
 echo "Done."
