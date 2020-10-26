066740a86071b1188b09c772fb677fa5b9581a87
Greg Pauloski <gpauloski@yahoo.com>
2020-10-26 12:25:23 -0500

    WIP: HYBRID_OPT fails on certain fractions
    Former-commit-id: f237c0269e10eaea8c8617b838a8f812544183ef

--- kfac/utils.py
+++ kfac/utils.py
@@ -1,4 +1,79 @@
 import torch
+import math
+
+from kfac import comm
+
+
+class WorkerAllocator(object):
+    def __init__(self, size, compute_grad_fraction):
+        self.size = size
+        self.compute_grad_fraction = compute_grad_fraction
+        # fraction is fraction of ranks in grad group
+        self.bcast_grad_ranks = self.partition_grad_ranks(size,
+                compute_grad_fraction)
+        self.bcast_inv_ranks = self.partition_inv_ranks(size,
+                compute_grad_fraction)
+        self.bcast_inv_groups = [self.make_group(ranks) 
+                for ranks in self.bcast_inv_ranks]
+        self.bcast_grad_groups = [self.make_group(ranks)
+                for ranks in self.bcast_grad_ranks]
+
+    @property
+    def grad_groups(self):
+        return len(self.bcast_grad_groups)
+
+    @property
+    def inv_groups(self):
+        return len(self.bcast_inv_groups)
+
+    # TODO(gpauloski): make not static
+    @staticmethod
+    def partition_grad_ranks(size, compute_grad_fraction):
+        grad_count = max(1, round(size * compute_grad_fraction))
+        inv_count = math.ceil(size / grad_count)
+        return [[j for j in range(i*grad_count, min((i+1)*grad_count, size))]
+                for i in range(0, inv_count)]
+
+    @staticmethod
+    def partition_inv_ranks(size, compute_grad_fraction):
+        grad_count = max(1, round(size * compute_grad_fraction))
+        inv_count = math.ceil(size / grad_count)
+        return [[j for j in range(i, size, grad_count)]
+                for i in range(0, grad_count)]
+
+    def make_group(self, ranks):
+        return comm.BroadcastGroup(ranks)
+
+    def get_grad_groups(self, src_ranks):
+        pairs = []
+        for rank in range(self.size):
+            idx = self._get_list_index(rank, self.bcast_grad_ranks)
+            grad_ranks = self.bcast_grad_ranks[idx]
+            # src is the intersection of the src_ranks (ranks that compute the
+            # gradients) and the broadcast group for the current rank
+            src = list(set(src_ranks) & set(grad_ranks))[0]
+            pairs.append((src, self.get_grad_group(rank)))
+        return pairs
+
+    def get_grad_ranks(self, rank):
+        idx = self._get_list_index(rank, self.bcast_grad_ranks)
+        return self.bcast_grad_ranks[idx]
+
+    def get_inv_ranks(self, rank):
+        idx = self._get_list_index(rank, self.bcast_inv_ranks)
+        return self.bcast_inv_ranks[idx]
+
+    def get_inv_group(self, rank):
+        idx = self._get_list_index(rank, self.bcast_inv_ranks)
+        return self.bcast_inv_groups[idx]
+
+    def get_grad_group(self, rank):
+        idx = self._get_list_index(rank, self.bcast_grad_ranks)
+        return self.bcast_grad_groups[idx]
+
+    def _get_list_index(self, item, nested_list):
+        return [i for i, sub_list in enumerate(nested_list)
+                if item in sub_list][0]
 
 
 def try_contiguous(x):
