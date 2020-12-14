12cce173dd7452692cba862b899cdd17b845ada1
Greg Pauloski <gpauloski@yahoo.com>
2020-12-14 11:00:40 -0600

    UPDATE: setup.py
    Former-commit-id: 8adabd8645be7c01667e5ddab93c682381d13b8a

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ setuptools.setup(
     version="0.3.1",
     author="Greg Pauloski",
     author_email="jgpauloski@uchicago.edu",
-    description="Distributed K-FAC Preconditioner for PyTorch + Horovod",
+    description="Distributed K-FAC Preconditioner for PyTorch",
     long_description=open('README.md').read(),
     url="https://github.com/gpauloski/kfac_pytorch",
     packages=setuptools.find_packages(),
+    license='MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
