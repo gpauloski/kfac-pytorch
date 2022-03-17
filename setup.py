6eb6815fe0989cef46e958ebc5b65424a9138c94
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2022-03-17 08:57:29 -0700

    Add CI files

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
-import setuptools
+"""Build kfac-pytorch package."""
+from setuptools import setup
 
-setuptools.setup(
-    name="kfac-pytorch",
-    version="0.4.0",
-    author="Greg Pauloski",
-    author_email="jgpauloski@uchicago.edu",
-    description="Distributed K-FAC Preconditioner for PyTorch",
-    long_description=open("README.md").read(),
-    url="https://github.com/gpauloski/kfac_pytorch",
-    packages=setuptools.find_packages(),
-    license="MIT",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
-    install_requires=[
-        "torch >= 1.8",
-    ],
-)
+setup()
